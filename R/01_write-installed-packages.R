d41e3cd80339e0e72b389147b91369bd0a089eb9
chayajones <chaya@chaya.org>
2018-02-01 11:41:40 -0800

    updated installed-packages, re-generated data file

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -1,6 +1,13 @@
 ## deja vu from yesterday!
 
+library(tidyverse)
+library(here)
+library(devtools)
+
 ## create a data frame of your installed packages
+
+inst_pkg <- installed.packages() %>% as_tibble()
+glimpse(inst_pkg)
 
 ## keep the variables
 ##   * Package
@@ -9,10 +16,15 @@
 ##   * Priority
 ##   * Built
 
+inst_pkg <- inst_pkg %>%
+  select(Package, LibPath, Version, Priority, Built)
+
 ## write it to data/installed-packages.csv
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 ## it an example of what yours should look like
+
+write_csv(inst_pkg, here("data", "installed-packages.csv"), append = FALSE)
 
 ## when this script works, stage & commit it and the csv file
 ## PUSH!
